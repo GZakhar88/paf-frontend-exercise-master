868d2d4327fb2a943e08d34c8b72165ad434124c
GZakhar88 <g.zakhar@gmail.com>
2021-08-27 17:59:59 +0200

    basic components implemented

--- src/scripts.tsx
+++ src/scripts.tsx
@@ -1,23 +1,83 @@
-import React from 'react';
+import * as React from 'react';
+import { FC } from 'react';
+import { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 
-const url: string = '/api/games/lists.json';
-  
-  // [X] 3. Fetch JSON-data from the following url: `/api/games/lists.json`
-async function fetchData(inputUrl: string) {
-    try {
-      const requestData = await fetch(inputUrl);
-      const data = await requestData.json();
-      // setLists(data)
-      console.log(data)
-    } catch (error) {
-      console.log(error)
-  }
-};
-  
-fetchData(url);
-  
+const App = () => {
 
+//------------------------------------TYPES-----------------------------------//
+  type data = {
+    title: string;
+    description: string;
+    lists: Array<list>
+  };
+  type item = {
+    id: number;
+    title: string;
+    provider: string;
+    image: string;
+  };
+  type list = {
+    id: string;
+    title: string;
+    items: Array<item>
+  };
+  
+//------------------------------------STATES-----------------------------------//
+  const [data, setData] = useState<data>();
+
+
+
+//------------------------------------FUNCTIONS-----------------------------------//
+  async function fetchData() {
+      try {
+        const requestData = await fetch('/api/games/lists.json');
+        const response: data = await requestData.json();
+        setData(response)
+      } catch (error) {
+        console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+
+//------------------------------------COMPONENTS-----------------------------------//  
+  const SingleItem: FC<item> = (props) => {
+    return (
+      <div className="single-item">
+        <img src={props.image} />
+        <p>{props.title}</p>
+      </div>
+    );
+  };
+
+  const ListItem: FC<list> = (props) => {
+    return (
+      <div className="list-item">
+        <h2>{props.title}</h2>
+        {props.items.map(element => {
+          return <SingleItem  key={element.id} {...element} />
+        })}
+      </div>
+    )
+  };
+
+  return (
+    <div className="app">
+      { data !== undefined && data.lists.map(element => {
+        return <ListItem  key={element.id} {...element}/>
+      })}
+    </div>
+  )
+  
+}
+  
+  
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
 
 
 
