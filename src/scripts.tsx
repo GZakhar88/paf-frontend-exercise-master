cf3ce06205c463968fcf6aba00b647e04295bb64
GZakhar88 <g.zakhar@gmail.com>
2021-08-28 22:55:03 +0200

    search implemented
    -search
    -10 recent search saved into localstorage

--- src/scripts.tsx
+++ src/scripts.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
-import { FC } from 'react';
+import { FC, FormEvent } from 'react';
 import { useState, useEffect } from 'react';
 import { render } from 'react-dom';
+const URL = '/api/games/lists.json'
 
-const App = () => {
 
-//------------------------------------TYPES-----------------------------------//
+const Components = () => {
+
+  //------------------------------------TYPES-----------------------------------//
   type data = {
     title: string;
     description: string;
     lists: Array<list>
   };
-  type item = {
+  type game = {
     id: number;
     title: string;
     provider: string;
@@ -20,31 +22,62 @@ const App = () => {
   type list = {
     id: string;
     title: string;
-    items: Array<item>
+    items: Array<game>
   };
   
-//------------------------------------STATES-----------------------------------//
+  //------------------------------------STATES-----------------------------------//
   const [data, setData] = useState<data>();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [foundedItem, setFoundedItem] = useState<game[]>([]);
+  const [recentSearch, setRecentSearch] = useState<string[]>([])
 
-
-
-//------------------------------------FUNCTIONS-----------------------------------//
-  async function fetchData() {
-      try {
-        const requestData = await fetch('/api/games/lists.json');
-        const response: data = await requestData.json();
-        setData(response)
-      } catch (error) {
-        console.log(error)
+  
+  useEffect(() => {
+    fetchData(URL);
+    const localStorageData = localStorage.getItem("recent");
+    if (localStorageData) {
+      setRecentSearch(JSON.parse(localStorageData))
     }
-  };
+  }, []);
+  
+  useEffect(() => {
+    searchTerm.length !== 0 && searchInData(searchTerm)
+  }, [searchTerm])
 
   useEffect(() => {
-    fetchData()
-  }, []);
+    localStorage.setItem('recent', JSON.stringify(recentSearch))
+  })
+  
+  //------------------------------------FUNCTIONS-----------------------------------//
+  async function fetchData(inputUrl: string) {
+    try {
+      const requestData = await fetch(inputUrl);
+      const response: data = await requestData.json();
+      setData(response)
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  
+
+  const searchInData = (input: string) => {
+    const searchedItem: game[] = [];
+    data?.lists.map(list => {
+      list.items.map(item => {
+        item.title == input && searchedItem.push(item);
+      })
+    });
+    searchedItem.length === 0 && alert('Sorry but not found any game with this name');
+    const filtered = [...new Map(searchedItem.map(item => [JSON.stringify(item), item])).values()];
+    setFoundedItem(filtered)
+  };
+
+  
+
+  
 
 
-//------------------------------------COMPONENTS-----------------------------------//
+  //------------------------------------REACT COMPONENTS-----------------------------------//
   const Header: FC<data> = (props) => {
     return (
       <div className="list-header">
@@ -55,10 +88,10 @@ const App = () => {
     );
   };
 
-  const SingleItem: FC<item> = (props) => {
+  const SingleItem: FC<game> = (props) => {
     return (
       <div className="single-item">
-        <img className="game-logo" src={props.image} />
+          <img className="game-logo" src={props.image} />
         <p>{props.title}</p>
       </div>
     );
@@ -71,28 +104,86 @@ const App = () => {
           <h2>{props.title}</h2>
         </div>
         <div className="games-container">
-        {props.items.map(element => {
-          return <SingleItem  key={element.id} {...element} />
-        })}
+          {props.items.map(element => {
+            return <SingleItem key={element.id} {...element} />
+          })}
         </div>
       </div>
     )
   };
 
-  return (
-    <div className="app">
-      { data !== undefined && <Header {...data}/>}
-      { data !== undefined && data.lists.map(element => {
-        return <CategoryItem  key={element.id} {...element}/>
-      })}
-    </div>
-  )
-  
-}
-  
+  //####################### SEARCH AND FILTER #######################
+  const Nav: FC = () => {
 
-const listElement = document.getElementById("list");
-render(<App />, listElement);
+    const Search: FC = () => {
+      const searchFunction = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement
+        const input = form.querySelector('#search-text') as HTMLInputElement
+        setSearchTerm(input.value);
+
+        if (recentSearch.length === 10) {
+          const tempArray = recentSearch
+          tempArray.shift()
+          setRecentSearch(tempArray);
+        }
+        setRecentSearch([...recentSearch, input.value]);
+      };
+
+     
+      return (
+        <div id="search">
+          <form className="search-form" onSubmit={event => searchFunction(event)}>
+            <input id="search-text" type="text" placeholder="Search..." autoComplete="off"/>
+            <button>Search</button>
+          </form>
+        </div>
+      )
+    };
+
+    const Filter: FC = () => {
+      return (
+        <div>
+
+        </div>
+      )
+    }
+
+
+    return (
+      <nav className="nav-bar-components">
+        <Search />
+        {/* <Filter /> */}
+      </nav>
+    )
+  };
+
+
+  // MAIN RETURN
+  return (
+    <div>
+      <Nav />
+      {data !== undefined && <Header {...data} />}
+      <div className="list">
+      { foundedItem.length !== 0
+        ? foundedItem.map(game => {
+          return <SingleItem key={game.id} {...game} />
+          })
+        : data !== undefined && data.lists.map(element => {
+            return <CategoryItem key={element.id} {...element} />
+          })}
+        </div>  
+    </div>
+  );
+};
+const componentsElement = document.getElementById("components");
+render(<Components />, componentsElement);
+
+
+
+
+
+
 
 
 
